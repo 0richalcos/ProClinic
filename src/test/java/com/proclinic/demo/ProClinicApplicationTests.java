0fbe46b7f5407528088b414000eb4d9e6b7547f6
Orichalcos <1943097043.com>
2020-07-06 11:38:11 +0800

    initialize

---
+++ src/test/java/com/proclinic/demo/ProClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proclinic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
