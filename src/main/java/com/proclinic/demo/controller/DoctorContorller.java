4d3e996c686b92b32636b7e0620588cafed88b6c
Asus <Asus@DESKTOP-5B2AP4O>
2020-07-10 14:30:36 +0800

    Doctor

--- src/main/java/com/proclinic/demo/controller/DoctorContorller.java
+++ src/main/java/com/proclinic/demo/controller/DoctorContorller.java
@@ -7,11 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.proclinic.demo.entity.UserDesc;
 import com.proclinic.demo.repository.DoctorRepository;
@@ -23,21 +28,32 @@ public class DoctorContorller {
 	DoctorRepository doctorrepository;
 	@Autowired
 	DoctorService doctorservce;
-	@GetMapping("/user/doctor")
+
+
+	@GetMapping("/doctor")
 	public Page<UserDesc> list(@RequestParam(name="p",defaultValue = "0") int page,@RequestParam(name="s",defaultValue = "2") int size) {
 		return doctorrepository.findAll(PageRequest.of(page, size,Sort.by("id").descending()));
 	}
-	@GetMapping("/user/doctor/{id}")
-	public UserDesc save(int id) {
-		return doctorservce.findAllBy(id);
+	@GetMapping("/doctor/{id}")
+	public ModelAndView save(ModelAndView mva,@PathVariable int id) {
+		mva.addObject("finby",doctorservce.findAllBy(id));
+		mva.setViewName("");
+		return mva;
 	}
-	@PostMapping("/admin/doctor")
-	public String add(UserDesc userdesc,String username,String departmentname) {
-		return doctorservce.adddoctor(userdesc,username,departmentname);
+	//X_X
+	@CrossOrigin
+	@PostMapping("/doctor")
+	public String add(@RequestBody UserDesc userdesc) {
+		return doctorservce.adddoctor(userdesc);
 	}
-	@DeleteMapping("/admin/doctor")
-	public boolean dele(List<String> list) {
-		return doctorservce.deletedoctor(list);
-		
+	//X_X
+	@DeleteMapping("/doctor")     
+	public boolean dele(@RequestBody String[] list) {
+	return doctorservce.deletedoctor(list);
+	}
+	//X_X
+	@PutMapping("/doctor/{id}")
+	public String upde(@RequestBody UserDesc userdesc,@PathVariable int id) {
+		return doctorservce.updatedoctor(userdesc,id);
 	}
 }
--- src/main/java/com/proclinic/demo/mapper/DoctorMapper.java
+++ src/main/java/com/proclinic/demo/mapper/DoctorMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.data.repository.query.Param;
 
 import com.proclinic.demo.entity.Department;
 import com.proclinic.demo.entity.User;
@@ -13,21 +14,24 @@ import com.proclinic.demo.entity.UserDesc;
 @Mapper
 public interface DoctorMapper {
 	//查询医生信息
-	@Select("select * from user_desc where id=#{id}")
-	UserDesc DoctorInformation(int id);
-	//修改医生信息
-	@Update("UPDATE user_desc SET specialization=#{specialization},experience=#{experience},gender=#{gender},birthday=#{birthday},age=#{age},email=#{email},details=#{details},address=#{address},phone=#{phone},user_id=#{user.id},department_id=#{department.id} WHERE id=#{id}")
-	void Doctorup(UserDesc userdesc);
-	//查询医生管理员信息	
-	@Select("select * from user where username=#{name}")
-	User Doctorid(String name);
-	//查询医生部门信息
-	@Select("select * from department where name=#{department}")
-	Department Doctordepartment(String department);
-	//添加医生
-	@Insert("INSERT INTO user_desc(specialization,experience,gender,birthday,age,email,details,address,phone,user_id,department_id) VALUES(#{specialization},#{experience},#{gender},#{birthday},#{age},#{email},#{details},#{address},#{phone},#{user_id},#{department_id})")
-	void Doctoradd(UserDesc userdesc);
-	//删除医生
-	@Delete("delete from user_desc where id=#{id}")
-	void Doctordelete(int id);
+		@Select("select * from user_desc where id=#{id}")
+		UserDesc DoctorInformation(int id);
+		//修改医生信息
+		@Update("update user_desc SET specialization=#{userdesc.specialization},experience=#{userdesc.experience},gender=#{userdesc.gender},birthday=#{userdesc.birthday},age=#{userdesc.age},details=#{userdesc.details},address=#{userdesc.address},phone=#{userdesc.phone},user_id=#{userdesc.user.id},department_id=#{userdesc.department.id} WHERE id=#{id}")
+		void Doctorup(@Param("userdesc")UserDesc userdesc,@Param("id") int id);
+		//查询医生管理员信息	
+		@Select("select * from user where username=#{name}")
+		User Doctorid(String name);
+		//查询医生部门信息
+		@Select("select * from department where name=#{department}")
+		Department Doctordepartment(String department);
+		//查询医生是否已存在
+		@Select("select * from user_desc where user_id=#{id}")
+		UserDesc doctorname(int id);
+		//添加医生
+		@Insert("INSERT INTO user_desc(specialization,experience,gender,birthday,age,details,address,phone,user_id,department_id) VALUES(#{specialization},#{experience},#{gender},#{birthday},#{age},#{details},#{address},#{phone},#{user.id},#{department.id})")
+		void Doctoradd(UserDesc userdesc);
+		//删除医生
+		@Delete("delete from user_desc where id=#{id}")
+		void Doctordelete(int id);
 }
