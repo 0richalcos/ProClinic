1dbb97a9dc252472f47e57b64e44633097e4e06c
Orichalcos <1943097043.com>
2020-07-10 11:54:06 +0800

    。

--- src/main/java/com/proclinic/demo/repository/UserRepository.java
+++ src/main/java/com/proclinic/demo/repository/UserRepository.java
@@ -2,15 +2,13 @@
 package com.proclinic.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.proclinic.demo.entity.User;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.proclinic.demo.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(nativeQuery = true, value = "select * from user where username=?1")
     User findUserByUserName(String username);
 }
-
